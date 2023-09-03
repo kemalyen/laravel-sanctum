c371ab03c703cdbc2d9ba874d9df3fa738a68ec1
Kemal Yenilmez <kemalyen@gmail.com>
2023-09-03 10:38:15 +0100

    update method added

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Product;
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Sanctum\Sanctum;
 
 beforeEach(function () {
@@ -16,7 +17,6 @@ it('get all products', function () {
     $response->assertStatus(200)
         ->assertJson(['message' => 'Products successfully fetched.'])
         ->assertJsonCount($count, 'data');
-
 })->group('product');
 
 it('has product page', function () {
@@ -41,7 +41,6 @@ it('creates a product', function () {
         ->title->toBe($sample['title']);
 })->group('product');
 
-
 it('response is a valid eloquent resource', function () {
     $sample = [
         'title' => fake()->word(),
@@ -53,4 +52,26 @@ it('response is a valid eloquent resource', function () {
     $response->assertStatus(201)
         ->assertJson(['message' => 'Product successfully created.'])
         ->assertJson(['data' => $sample]);
+})->group('product');
+
+it('update a product', function () {
+
+    Product::factory()->create();
+    $product = Product::latest()->first();
+
+    $sample = [
+        'title' => fake()->word(),
+        'description' => fake()->text(200),
+        'price' => fake()->randomNumber(2, false)
+    ];
+
+    $response = $this->put('/api/products/' . $product->id, $sample);
+    $response->assertStatus(200);
+
+    expect(Product::latest()->first())
+        ->title->toBeString()->not->toBeEmpty()
+        ->description->toBeString()->not->toBeEmpty()
+        ->price->toBeNumeric()
+        ->title->toBe($sample['title'])
+        ->title->not->toBe($product->title);
 })->group('product');
